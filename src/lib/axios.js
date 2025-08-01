5fb28029b6e67d35ad754dd098c533d51c70d725
Your Name <your-email@example.com>
2025-08-01 10:00:24 +0530

    third

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://read-nest-backend-indol.vercel.app/api',
   withCredentials: true,
 });
 
